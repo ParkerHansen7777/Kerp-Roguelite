9fc8a20abc6f3131cb2906b546c10a3122e518d3
ParkerHansen7777 <parkerhansen7777@gmail.com>
2024-01-16 11:26:47 -0800

    Update PlayerMovementScript.cs

--- Assets/PlayerMovementScript.cs
+++ Assets/PlayerMovementScript.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public float moveSpeed = 10;
+
+
+
     void Update()
     {
+
+        if (Input.GetKey(KeyCode.W) & Input.GetKey(KeyCode.A)){
+            transform.position += (Vector3.up * moveSpeed * Time.deltaTime / 2) + (Vector3.right * -moveSpeed * Time.deltaTime / 2);
+        }
+
+        else if (Input.GetKey(KeyCode.W) & Input.GetKey(KeyCode.D))
+        {
+            transform.position += (Vector3.up * moveSpeed * Time.deltaTime / 2) + (Vector3.right * moveSpeed * Time.deltaTime / 2);
+        }
+
+        else if (Input.GetKey(KeyCode.S) & Input.GetKey(KeyCode.A))
+        {
+            transform.position += (Vector3.up * -moveSpeed * Time.deltaTime / 2) + (Vector3.right * -moveSpeed * Time.deltaTime / 2);
+        }
+
+        else if (Input.GetKey(KeyCode.S) & Input.GetKey(KeyCode.D))
+        {
+            transform.position += (Vector3.up * -moveSpeed * Time.deltaTime / 2) + (Vector3.right * moveSpeed * Time.deltaTime / 2);
+        }
+
+        else if (Input.GetKey(KeyCode.W)) {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        }
+        
+        else if (Input.GetKey(KeyCode.A)) {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+        
+        else if (Input.GetKey(KeyCode.D)){
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        }
+        
+        else if (Input.GetKey(KeyCode.S)){
+            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+
+        }
         
     }
+        
+    
 }
